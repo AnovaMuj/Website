cf6615d6f019ec1dcf54fe1e07a8a5915b1e1987
AnovaMuj <146035797+AnovaMuj@users.noreply.github.com>
2023-10-27 19:13:20 +0530

    Update file path

--- src/HomePage/HeroSection.jsx
+++ src/HomePage/HeroSection.jsx
@@ -3,14 +3,14 @@ import "./HeroSection.css";
 function HeroSection() {
   function loadParticles() {
     const scriptTag = document.createElement("script");
-    scriptTag.src = "/src/HomePage/particle-image.min.js";
+    scriptTag.src = "src/HomePage/particle-image.min.js";
     scriptTag.setAttribute("defer", "defer");
     document.body.appendChild(scriptTag);
   }
 
   return (
     <div className="HeroContainer">
-      <div id="particle-image" data-params-src="/src/HomePage/params.json"></div>
+      <div id="particle-image" data-params-src="src/HomePage/params.json"></div>
       {loadParticles()}
     </div>
   );
