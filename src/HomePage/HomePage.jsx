70346a658e7d1a585cad02f601d2c8af64ce3da2
AnovaMuj <146035797+AnovaMuj@users.noreply.github.com>
2023-10-27 20:21:01 +0530

    Update HomePage.jsx

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -6,78 +6,12 @@ import PastEvents from "./PastEvents";
 import UpcomingEvents from "./UpcomingEvents";
 import Footer from "./Footer";
 import "./HomePage.css";
+import Parallax from "./Parallax.js";
+import BackgroundStars from "./BackgroundStars.js";
 
 function HomePage() {
-  setTimeout(() => {$.fn.moveIt = function () {
-  var $window = $(window);
-  var instances = [];
-
-  $(this).each(function () {
-    instances.push(new moveItItem($(this)));
-  });
-
-  window.onscroll = function () {
-    var scrollTop = $window.scrollTop();
-    instances.forEach(function (inst) {
-      inst.update(scrollTop);
-    });
-  };
-};
-
-var moveItItem = function (el) {
-  this.el = $(el);
-  this.speed = parseInt(this.el.attr("data-scroll-speed"));
-};
-
-moveItItem.prototype.update = function (scrollTop) {
-  this.el.css("transform", "translateY(" + -(scrollTop / this.speed) + "px)");
-};
-
-$(function () {
-  $("[data-scroll-speed]").moveIt();
-});
-
-
-  let sky = document.querySelector(".HomePageBgLayer1");
-
-var body = document.body,
-  html = document.documentElement;
-
-var PageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-var PageWidth = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
-
-function createDiv(size) {
-  let circle = document.createElement("div");
-  circle.classList.add("Stars");
-
-  let randRange5 = Math.floor(Math.random() * 5) + 1;
-  circle.classList.add(`blink_${randRange5}`);
-
-  let widthAndHeight = random(size, "px");
-  circle.style.height = circle.style.width = widthAndHeight;
-
-  circle.style.left = random(PageWidth, "px");
-  circle.style.top = random(PageHeight, "px");
-
-  sky.appendChild(circle);
-}
-
-function paintStars(stars, size) {
-  while (sky.firstChild) {
-    sky.removeChild(sky.firstChild);
-  }
-  for (let i = 0; i < stars; i++) {
-    createDiv(size);
-  }
-}
-
-function random(range, unit) {
-  let randNum = Math.floor(Math.random() * range) + 1;
-  return `${randNum}${unit}`;
-}
-
-paintStars(400, 5)}, 1000});
-
+  {Parallax()}
+  {BackgroundStars()}
 
   return (
     <>
