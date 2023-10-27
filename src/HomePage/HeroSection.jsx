ee31382ce6841260ac3c88fa75c5f613d06f9268
AnovaMuj <146035797+AnovaMuj@users.noreply.github.com>
2023-10-28 03:54:29 +0530

    Update HeroSection.jsx

--- src/HomePage/HeroSection.jsx
+++ src/HomePage/HeroSection.jsx
@@ -10,7 +10,7 @@ function HeroSection() {
 
   return (
     <div className="HeroContainer">
-      <div id="particle-image" data-params-src="https://cdn.jsdelivr.net/gh/AnovaMuj/Website/src/HomePage/param.json"></div>
+      <div id="particle-image" data-params-src="https://cdn.jsdelivr.net/gh/AnovaMuj/Website/src/HomePage/para.json"></div>
       {loadParticles()}
     </div>
   );
