0348cc077ff5bd90104305065eead6bd6162ed3e
AnovaMuj <146035797+AnovaMuj@users.noreply.github.com>
2023-10-28 19:09:24 +0530

    Update Execs.jsx

--- src/HomePage/Execs.jsx
+++ src/HomePage/Execs.jsx
@@ -5,7 +5,7 @@ import "./Execs.css";
 const Execs = () => {
   return (
     <>
-      <h1 className="HomeTitle">Execs</h1>
+      <h1 className="HomeTitle">Executive Committee</h1>
       <div className="ExecContainer">
         {ExecDetails.map((Exec, Index) => (
           <ExecCards Picture={Exec.Picture} Name={Exec.Name} About={Exec.About} Insta={Exec.Insta} Mail={Exec.Mail} Contact={Exec.Contact} key={Index} />
